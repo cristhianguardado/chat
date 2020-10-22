9591560340a50bc97f7836cd6376326628833bd2
cristhianguardado <cristhian.estrada@tec.mx>
2020-10-22 09:57:18 -0500

    primera version

---
+++ server/routes/index.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const userRoutes = require('./user.route');
+
+const router = express.Router(); // eslint-disable-line new-cap
+
+/** GET /health-check - Check service health */
+router.get('/health-check', (req, res) =>
+  res.send('OK')
+);
+
+
+router.use('/user', userRoutes);
+
+module.exports = router;
---
+++ server/routes/user.route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const userCtrl = require('../controllers/user.controller');
+
+const router = express.Router();
+module.exports = router;
+
+router.route('/')
+  .post(asyncHandler(insert));
+
+
+async function insert(req, res) {
+  let user = await userCtrl.insert(req.body);
+  res.json(user);
+}
